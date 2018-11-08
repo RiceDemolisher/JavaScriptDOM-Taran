82efa1a0e808d538918cf12268f5e73c581ae10f
Taran Singh <taran.jhattu@gmail.com>
2018-11-08 11:21:58 +0100

    JavascriptDOM-Taran

---
+++ javascript.js
@@ -0,0 +1,6 @@
+
+document.getElementById("Paginakop").innerHTML = "Dit is de paginatitel";
+document.getElementById("Subkop").innerHTML = "Dit is de subkop";
+document.getElementById("aap1").src = "img/lammetje.png";
+document.getElementById("aap2").src = "img/David Hasselhoff.png";
+
---
+++ javascript2.js
@@ -0,0 +1,19 @@
+let divje1 = document.getElementById("bl1");
+divje1.style.backgroundColor = " blue ";
+divje1.style.paddingTop = "15px";
+divje1.style.paddingLeft = "21px";
+
+
+let divje2 = document.getElementById("bl2");
+divje2.style.backgroundColor = "green";
+divje2.style.marginTop = "14px";
+divje2.style.paddingLeft = "21px";
+
+let divje3 = document.getElementById("bl3");
+divje3.style.backgroundColor = "yellow";
+divje3.style.marginTop = "35px";
+divje3.style.paddingLeft = "45px";
+
+let divje4 = document.getElementById("bl4");
+divje4.style.backgroundColor = "red";
+
